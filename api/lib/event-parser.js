dc4da1c2272e7f2b0603d01413150ddffc3814ae
George Kando <gkando@gmail.com>
2021-03-30 21:25:34 +0200

    [skip actions] event-parser fix - changed in lambda console so skipping this build.

--- api/lib/event-parser.js
+++ api/lib/event-parser.js
@@ -5,20 +5,19 @@ const eventSchema = Joi.object({
   playerId: Joi.string().alphanum().required(),
   kills: Joi.number().min(1).required(),
 });
-const extractEvent = (event) => event.body[0];
 
-exports.parse = async (event) => {
-  console.log("EVENT BODY", event.body);
+exports.parse = async (body) => {
+  console.log("EVENT BODY", body);
   // If we do not receive a body, we cannot continue...
-  if (!event.body) {
+  if (!body) {
     // ...so we return a Bad Request response
     throw Error(response.json("Missing game events data.", 400));
   }
   // validate the first item in the events array
   try {
-    const eventObject = extractEvent(event);
+    const eventObject = body[0];
     const { value: payload, error } = eventSchema.validate(eventObject);
-    return event.body;
+    return body;
   } catch (error) {
     throw Error(`Payload error => ${error}.`);
   }
